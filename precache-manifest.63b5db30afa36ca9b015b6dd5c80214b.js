d9e8cddd8a4c7a2c19aa9249e8aafbc3485a4b3b
Mario Hidalgo García <MarioHidalgoTi@gmail.com>
2019-02-14 17:44:52 -0300

    Example

---
+++ precache-manifest.63b5db30afa36ca9b015b6dd5c80214b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a50d8540f2cbefeb18e9",
+    "url": "/static/js/main.a50d8540.chunk.js"
+  },
+  {
+    "revision": "9d51dda1dc174f663998",
+    "url": "/static/js/2.9d51dda1.chunk.js"
+  },
+  {
+    "revision": "a50d8540f2cbefeb18e9",
+    "url": "/static/css/main.15fba789.chunk.css"
+  },
+  {
+    "revision": "9dad8e5b21be7aff6034e12a83e5b1b8",
+    "url": "/index.html"
+  }
+];
